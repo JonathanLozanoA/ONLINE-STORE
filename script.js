5721ebaaae6af8caf7d7680dcdf8492ad11b0cd4
jonathan lozano <jonathanlozzano@gmail.com>
2024-08-07 12:36:30 -0500

    Se actualiza el home

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-// FUNCIÓN PARA MODIFICAR EL BOTÓN DE ROPASPORT E IMAGEN DEL HOME
-function ropaSport() {
-  let el = document.getElementById("ropaSport"); 
-  el.innerHTML = "¡Buena elección!";
-  //Modifica el banner bajo el title
-  let el1 = document.querySelector("p");
-  el1.innerHTML = "VAMOS AL CARRITO DE COMPRAS"; 
-  let el2 = document.querySelector(".imgRopaCasual");
-  el2.src = "https://st3.depositphotos.com/9880800/14298/i/450/depositphotos_142987593-stock-photo-sporty-men-and-women.jpg";
-}
+// // FUNCIÓN PARA MODIFICAR EL BOTÓN DE ROPASPORT E IMAGEN DEL HOME
+// function ropaSport() {
+//   let el = document.getElementById("ropaSport"); 
+//   el.innerHTML = "¡Buena elección!";
+//   //Modifica el banner bajo el title
+//   let el1 = document.querySelector("p");
+//   el1.innerHTML = "VAMOS AL CARRITO DE COMPRAS"; 
+//   let el2 = document.querySelector(".imgRopaCasual");
+//   el2.src = "https://st3.depositphotos.com/9880800/14298/i/450/depositphotos_142987593-stock-photo-sporty-men-and-women.jpg";
+// }
    
-// FUNCIÓN PARA MODIFICAR EL BOTÓN DE ROPASPORT E IMAGEN DEL HOME
-function altoRendimiento() {
-  document.querySelector("#altoRendimiento").innerHTML = "¡WOW, que nivel!";
-  let el = document.querySelector(".imgRopaAltoRendimiento");
-  el.src = "https://estaticos-cdn.prensaiberica.es/clip/34c86ea8-c1b3-4d13-a7fc-a1b5ee320dd7_alta-libre-aspect-ratio_default_0.jpg";
-} 
+// // FUNCIÓN PARA MODIFICAR EL BOTÓN DE ROPASPORT E IMAGEN DEL HOME
+// function altoRendimiento() {
+//   document.querySelector("#altoRendimiento").innerHTML = "¡WOW, que nivel!";
+//   let el = document.querySelector(".imgRopaAltoRendimiento");
+//   el.src = "https://estaticos-cdn.prensaiberica.es/clip/34c86ea8-c1b3-4d13-a7fc-a1b5ee320dd7_alta-libre-aspect-ratio_default_0.jpg";
+// } 
 
 //FUNCIÓN PARA SABER EL NUMERO DE CATEGORÍA DISPONIBLES
 function numeroItems() {
